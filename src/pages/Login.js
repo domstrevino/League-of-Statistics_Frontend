667515d5035ca63337e22b2c5503483ed087b926
dominick <domstrevino@aol.com>
2022-09-09 14:38:01 -0400

    adding buttonj colors for each region

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,59 @@ import { useState } from 'react';
 
 function Login() {
     const [region, setRegion] = useState('NA');
+    const [color, setColor] = useState('#ff9b00');
+
+    const changeCSS = (region) => {
+        switch(region) {
+            case 'NA':
+                setRegion('NA');
+                setColor('#ff9b00');
+                break;
+            case 'EUW':
+                setRegion('EUW');
+                setColor('#1e48a8');
+                break;
+            case 'EUNE':
+                setRegion('EUNE');
+                setColor('#3374a9');
+                break;
+            case 'KR':
+                setRegion('KR');
+                setColor('#4d2ac0');
+                break;
+            case 'JP':
+                setRegion('JP');
+                setColor('#ec5d8c');
+                break;
+            case 'BR':
+                setRegion('BR');
+                setColor('#6cd7a2');
+                break;
+            case 'RU':
+                setRegion('RU');
+                setColor('#bf2828');
+                break;
+            case 'OCE':
+                setRegion('OCE');
+                setColor('#8eced5');
+                break;
+            case 'TR':
+                setRegion('TR');
+                setColor('#ec5c57');
+                break;
+            case 'LAN':
+                setRegion('LAN');
+                setColor('#f7c544');
+                break;
+            case 'LAS':
+                setRegion('LAS');
+                setColor('#ed642b');
+                break;
+            default:
+                setRegion('NA');
+                setColor('#ff9b00');
+        }
+    }
 
     return (
         <section className="Login">
@@ -18,31 +71,31 @@ function Login() {
                 />
                 <Card.Body className = 'Login-Body'>
                     <Card.Title as = 'h4'>
-                        Login
+                        Search Yourself or a Champion
                     </Card.Title>
                     <Card.Text as = 'div' className = 'Login-Text'>
                         <form className = 'Login-Form'>
                             <Dropdown>
-                                <Dropdown.Toggle className = 'Login-Button'>{region}</Dropdown.Toggle>
+                                <Dropdown.Toggle className = 'Login-Button' style = {{backgroundColor: color}}>{region}</Dropdown.Toggle>
                                 <Dropdown.Menu>
-                                    <Dropdown.Item onClick = {() => setRegion('NA')}>North America (NA)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('EUW')}>European West (EUW)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('EUNE')}>European Nordic & East (EUNE)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('KR')}>Korea (KR)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('BR')}>Brazil (BR)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('JP')}>Japan (JP)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('RU')}>Russia (RU)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('OCE')}>Oceania (OCE)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('TR')}>Turkey (TR)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('LAN')}>Latin America North (LAN)</Dropdown.Item>
-                                    <Dropdown.Item onClick = {() => setRegion('LAS')}>Latin America South (LAS)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('NA')}>North America (NA)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('EUW')}>European West (EUW)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('EUNE')}>European Nordic & East (EUNE)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('KR')}>Korea (KR)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('BR')}>Brazil (BR)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('JP')}>Japan (JP)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('RU')}>Russia (RU)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('OCE')}>Oceania (OCE)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('TR')}>Turkey (TR)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('LAN')}>Latin America North (LAN)</Dropdown.Item>
+                                    <Dropdown.Item onClick = {() => changeCSS('LAS')}>Latin America South (LAS)</Dropdown.Item>
                                 </Dropdown.Menu>
                             </Dropdown>
                             <input 
                             className = 'Login-Input'
                             type = 'text'
                             id = 'username'
-                            placeholder = 'Username...'
+                            placeholder = 'IGN...'
                             />
                         </form>
                     </Card.Text>

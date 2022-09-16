de9b32856e47262d78376b98075489b51b38dc70
dominick <domstrevino@aol.com>
2022-09-16 18:44:22 -0400

    configuring ability to send form data to backend API

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,56 +6,103 @@ import { useState } from 'react';
 
 function Login() {
     const [region, setRegion] = useState('NA');
+    const [regionQuery, setRegionQuery] = useState('na1');
     const [color, setColor] = useState('#ff9b00');
+    const [visibility, setVisibility] = useState('hidden');
+    const [name, setName] = useState('');
+
+    const checkName = async (e) => {
+        e.preventDefault();
+
+        const { username } = e.target.elements;
+        setName(username.value);
+        const user = { 
+            name: username.value,
+            region: regionQuery
+        };
+        
+
+        const result = await fetch(`https://localhost:44318/api/v1/Summoners?region=${user.region}&name=${user.name}`, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                setVisibility('visible');
+                return Promise.reject(response);
+            }
+        }).catch((error) => {
+            setVisibility('visible');
+            throw new Error(error);
+        });
+        //TODO: Remove alert and figure out how to send the JSON data to the next page
+        alert(result.id);
+    }
 
     const changeCSS = (region) => {
         switch(region) {
             case 'NA':
                 setRegion('NA');
+                setRegionQuery('na1');
                 setColor('#ff9b00');
                 break;
             case 'EUW':
                 setRegion('EUW');
+                setRegionQuery('euw1');
                 setColor('#1e48a8');
                 break;
             case 'EUNE':
                 setRegion('EUNE');
+                setRegionQuery('eun1');
                 setColor('#3374a9');
                 break;
             case 'KR':
                 setRegion('KR');
+                setRegionQuery('kr');
                 setColor('#4d2ac0');
                 break;
             case 'JP':
                 setRegion('JP');
+                setRegionQuery('jp1');
                 setColor('#ec5d8c');
                 break;
             case 'BR':
                 setRegion('BR');
+                setRegionQuery('br1');
                 setColor('#6cd7a2');
                 break;
             case 'RU':
                 setRegion('RU');
+                setRegionQuery('ru');
                 setColor('#bf2828');
                 break;
             case 'OCE':
                 setRegion('OCE');
+                setRegionQuery('oc1');
                 setColor('#8eced5');
                 break;
             case 'TR':
                 setRegion('TR');
+                setRegionQuery('tr1');
                 setColor('#ec5c57');
                 break;
             case 'LAN':
                 setRegion('LAN');
+                setRegionQuery('la1');
                 setColor('#f7c544');
                 break;
             case 'LAS':
                 setRegion('LAS');
+                setRegionQuery('la2');
                 setColor('#ed642b');
                 break;
             default:
                 setRegion('NA');
+                setRegionQuery('na1');
                 setColor('#ff9b00');
         }
     }
@@ -65,7 +112,7 @@ function Login() {
             <Card className = 'Login-Card'>
                 <Card.Img 
                 className = 'Login-Img' 
-                variant = 'top' 
+                variant = 'top'
                 src = {logo}
                 alt = 'Logo'
                 />
@@ -74,7 +121,7 @@ function Login() {
                         Search Yourself or a Champion
                     </Card.Title>
                     <Card.Text as = 'div' className = 'Login-Text'>
-                        <form className = 'Login-Form'>
+                        <form className = 'Login-Form' onSubmit={checkName}>
                             <Dropdown>
                                 <Dropdown.Toggle className = 'Login-Button' style = {{backgroundColor: color}}>{region}</Dropdown.Toggle>
                                 <Dropdown.Menu>
@@ -100,6 +147,10 @@ function Login() {
                         </form>
                     </Card.Text>
                 </Card.Body>
+                <section className = 'Login-Error' style = {{ visibility: visibility }}>
+                    <div>Could not retrieve data for <span className = 'Name'>"{name}"</span></div>
+                    <div>Double check your spelling or try a different region</div>
+                </section>
             </Card>
         </section>
     );
